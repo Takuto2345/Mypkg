778d89ff7d9804634c5a3f78347a6b214244d6f0
Takuto Kanno <s21C1038JU@s.chibakoudai.jp>
2022-12-08 11:37:10 +0900

    add a class

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,18 +1,24 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16 
+from std_msgs.msg import Int16
+
+class Talker():
+    def_init_(self):
+        self.pub = node.create_publisher(Int16, "countup", 10)
+        self.n = 0
 
 rclpy.init()
 node = Node("talker")
-pub = node.create_publisher(Int16, "countup", 10)
-n = 0
+talker = Talker()
+
+#pub = node.create_publisher(Int16, "countup", 10)
+#n = 0
 
 def cb():
-    global n
     msg = Int16()
-    msg.data = n
-    pub.publish(msg)
-    n += 1
+    msg.data = taler.n
+    talker.pub.publish(msg)
+    talker.n += 1
 
 node.create_timer(0.5,cb)
 rclpy.spin(node)
