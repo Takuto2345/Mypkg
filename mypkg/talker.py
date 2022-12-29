5ba175f40988916c5a343a2740395a85018dff77
Takuto Kanno <s21C1038JU@s.chibakoudai.jp>
2022-12-29 12:33:21 +0900

    delete a memo

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -3,7 +3,7 @@ from rclpy.node import Node
 from std_msgs.msg import Int16
 
 class Talker():
-    def __init__(self, node):#initに__（アンダーバー)が必要なのはselfとnodeに変更を設定できるようにするためであり、今回の場合一度初期値にするために必要らしい
+    def __init__(self, node):
         self.pub = node.create_publisher(Int16, "countup", 10)
         self.n = 0
         node.create_timer(0.5, self.cb)
@@ -22,11 +22,3 @@ node = Node("talker")
 talker = Talker(node)
 rclpy.spin(node)
 
-#def cb():
-#    msg = Int16()
-#   msg.data = talker.n
-#    talker.pub.publish(msg)
-#    talker.n += 1
-
-#node.create_timer(0.5,cb)
-#rclpy.spin(node)
