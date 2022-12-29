a6112f375500b3a14a7cecb3f84d31c1ee2fe81b
Takuto Kanno <s21C1038JU@s.chibakoudai.jp>
2022-12-29 16:37:11 +0900

    chelenge for class

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -5,11 +5,15 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
 
-def cb(msg):
-    global node
-    node.get_logger().info("Listen: %d" % msg.data)
+class Listener():
+    def __init__(self, node):
+        self.subscription = self.create_subscription(Int16, "countup", cb, 10)
+    self.subscription
+    def cb(self, msg):
+        global node
+        self.get_logger().info("Listen: %d" % msg.data)
 
 rclpy.init()
 node = Node("listener")
-pub = node.create_subscription(Int16, "countup", cb, 10)
+#pub = node.create_subscription(Int16, "countup", cb, 10)
 rclpy.spin(node)
