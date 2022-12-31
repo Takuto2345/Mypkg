b71fce5f5a31f3e4f9a1931bda1e7b23a02bc5c4
Takuto Kanno <s21C1038JU@s.chibakoudai.jp>
2022-12-31 11:10:01 +0900

    add a commit

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -8,8 +8,8 @@ from std_msgs.msg import Int16
 class Listener():
     def __init__(self, node):
         self.subscription = self.create_subscription(Int16, "countup", cb, 10)
-    self.subscription
-    def cb(self):
+        self.subscription
+    def cb(self,msg):
         self.get_logger().info("Listen: %d" % msg.data)
 
 def main():
